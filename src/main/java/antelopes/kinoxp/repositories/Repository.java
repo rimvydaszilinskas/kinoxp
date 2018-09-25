61db77967c3b58543f9a27f1c2fb2504cfb3a6e1
Rimvydas Zilinskas <rilinskas@yahoo.com>
2018-09-25 13:17:40 +0200

    Repository added

--- src/main/java/antelopes/kinoxp/repositories/Repository.java
+++ src/main/java/antelopes/kinoxp/repositories/Repository.java
@@ -1,4 +1,36 @@
 package antelopes.kinoxp.repositories;
 
-public class Repository {
+import antelopes.kinoxp.repositories.util.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class Repository<T> {
+    protected PreparedStatement preparedStatement;
+    protected ResultSet resultSet;
+    protected Connection connection;
+
+    public Repository(){
+        try {
+            connection = Database.getConnection();
+        } catch (SQLException err){
+            System.out.println("Failed to connect. Err: " + err.getSQLState());
+        }
+    }
+
+    public abstract T get(int id);
+
+    public abstract List<T> getAll();
+
+    public abstract boolean delete(T object);
+
+    public abstract boolean delete(int id);
+
+    public abstract boolean update(T object);
+
+    public abstract boolean create(T object);
+
 }
