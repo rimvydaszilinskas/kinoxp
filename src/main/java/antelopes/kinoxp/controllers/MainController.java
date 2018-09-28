80b152f258f93b96078ae482edf5ce4d06132c7f
Alexandra Caragata <caragata.alexa@yahoo.com>
2018-09-28 13:44:30 +0200

    Controller updated Html linked with the CSS

--- src/main/java/antelopes/kinoxp/controllers/MainController.java
+++ src/main/java/antelopes/kinoxp/controllers/MainController.java
@@ -7,11 +7,12 @@ import antelopes.kinoxp.repositories.CustomerRepository;
 import antelopes.kinoxp.repositories.MovieRepository;
 import antelopes.kinoxp.repositories.Repository;
 import antelopes.kinoxp.repositories.ReservationRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Controller
 public class MainController {
     private Repository<Customer> customerRepository;
     private Repository<Movie> movieRepository;
@@ -25,7 +26,23 @@ public class MainController {
 
     @GetMapping("/")
     public String index(){
-        return "/";
+        return "index.html"; }
 
+    @GetMapping("/employees/login.html")
+    public String login() {
+        return "employees/login";
+    }
+
+    @GetMapping("/customers/movieList.html")
+    public String movieList(){return "customers/movieList";}
+
+    @GetMapping("/customers/booking.html")
+    public String booking(){
+        return "customers/booking";
+    }
+
+    @GetMapping("/employees/employees.html")
+    public String employees(){
+        return "employees/employees";
     }
 }
