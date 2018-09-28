9482e0d43ae23d016bd1e60c02e0302c4f328534
Dragos Mocanasu <stilldragos10@gmail.com>
2018-09-28 12:34:10 +0200

    created seat repository

--- Database_config.sql
+++ Database_config.sql
@@ -3,19 +3,19 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers(
   id INT(5) NOT NULL PRIMARY KEY auto_increment,
-  name VARCHAR(45) NOT NULL
+  space VARCHAR(45) NOT NULL
 );
 
 DROP TABLE IF EXISTS genres;
 CREATE TABLE genres(
   id INT(5) NOT NULL auto_increment primary key,
-  name VARCHAR(45) NOT NULL
+  space VARCHAR(45) NOT NULL
 );
 
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
   id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(45) NOT NULL,
+  space VARCHAR(45) NOT NULL,
   genre INT(5) NOT NULL,
   age_limit INT(2) NOT NULL,
   FOREIGN KEY movies(genre) REFERENCES genres(id)
